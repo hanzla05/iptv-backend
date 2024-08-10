e215ae9977f0512ded10c3cc67dd683c2510e380
Hanzla Munir <imhanzla05@gmail.com>
2024-08-10 12:39:12 +0500

    db

--- config/db.js
+++ config/db.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
+const connectDB = async (retries = 5) => {
+    while (retries) {
+      try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+          serverSelectionTimeoutMS: 50000,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        break;
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        retries -= 1;
+        console.log(`Retries left: ${retries}`);
+        await new Promise(res => setTimeout(res, 5000)); // wait 5 seconds before retrying
+      }
+    }
+  
+    if (!retries) {
+      console.error('Failed to connect to MongoDB after multiple attempts');
+      process.exit(1);
+    }
+  };
+  
+  export default connectDB;
+  
 
-export default connectDB;
